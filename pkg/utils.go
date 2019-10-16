32ce77438a5a8097eea728debbbd0b8e474c27f9
Md. Emruz Hossain <hossainemruz@gmail.com>
2019-10-16 15:16:54 +0600

    Fix argument passing (#5)

--- pkg/utils.go
+++ pkg/utils.go
@@ -3,9 +3,11 @@ package pkg
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/appscode/go/log"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/kubernetes"
 	appcatalog_cs "kmodules.xyz/custom-resources/client/clientset/versioned"
 	"stash.appscode.dev/stash/pkg/restic"
@@ -50,4 +52,16 @@ func waitForDBReady(host string, port int32) {
 		log.Infoln("Waiting... database is not ready yet")
 		time.Sleep(5 * time.Second)
 	}
+}
+
+func containsArg(args []string, checklist sets.String) bool {
+	for i := range args {
+		a := strings.FieldsFunc(args[i], func(r rune) bool {
+			return r == '='
+		})
+		if checklist.Has(a[0]) {
+			return true
+		}
+	}
+	return false
 }
