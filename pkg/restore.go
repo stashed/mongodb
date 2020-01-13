f1d57a874b91bc36af779bd777e407daad505923
Tamal Saha <tamal@appscode.com>
2020-01-12 21:30:47 -0800

    Make output.json file writable to all users + Fix Flag types (#19)
    * Make output.json file writable to all users + Fix Flag types
    * Use Go 1.13 and kind v0.6.1
    * Use helm 3 in chart readme
    * Fix totalHosts update
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>
    Co-authored-by: Md. Emruz Hossain <hossainemruz@gmail.com>

--- pkg/restore.go
+++ pkg/restore.go
@@ -122,7 +122,7 @@ func NewCmdRestore() *cobra.Command {
 	cmd.Flags().StringVar(&opt.setupOptions.SecretDir, "secret-dir", opt.setupOptions.SecretDir, "Directory where storage secret has been mounted")
 	cmd.Flags().StringVar(&opt.setupOptions.ScratchDir, "scratch-dir", opt.setupOptions.ScratchDir, "Temporary directory")
 	cmd.Flags().BoolVar(&opt.setupOptions.EnableCache, "enable-cache", opt.setupOptions.EnableCache, "Specify whether to enable caching for restic")
-	cmd.Flags().IntVar(&opt.setupOptions.MaxConnections, "max-connections", opt.setupOptions.MaxConnections, "Specify maximum concurrent connections for GCS, Azure and B2 backend")
+	cmd.Flags().Int64Var(&opt.setupOptions.MaxConnections, "max-connections", opt.setupOptions.MaxConnections, "Specify maximum concurrent connections for GCS, Azure and B2 backend")
 
 	cmd.Flags().StringVar(&opt.defaultDumpOptions.Host, "hostname", opt.defaultDumpOptions.Host, "Name of the host machine")
 	cmd.Flags().StringVar(&opt.defaultDumpOptions.SourceHost, "source-hostname", opt.defaultDumpOptions.SourceHost, "Name of the host whose data will be restored")
