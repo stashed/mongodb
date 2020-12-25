bc3a9d3f7207b6d3bf900442b8c7170cfb33d174
lgtm <1gtm@users.noreply.github.com>
2020-12-25 02:47:55 -0800

    [cherry-pick] Speed up schema generation process (#663) (#672)
    /cherry-pick
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>
    
    Co-authored-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/fuzzer/fuzzer.go
+++ apis/installer/fuzzer/fuzzer.go
@@ -26,7 +26,7 @@ import (
 // Funcs returns the fuzzer functions for this api group.
 var Funcs = func(codecs runtimeserializer.CodecFactory) []interface{} {
 	return []interface{}{
-		func(s *v1alpha1.StashMongoDB, c fuzz.Continue) {
+		func(s *v1alpha1.StashMongodb, c fuzz.Continue) {
 			c.FuzzNoCustom(s) // fuzz self without calling this function again
 		},
 	}
