3af0fc62d6da2dc68187450b7adc2330a00e0479
1gtm <1gtm@appscode.com>
2022-03-29 18:47:13 +0000

    Prepare for release 3.6.8-v15
    ProductLine: Stash
    
    Release: v2022.03.29
    
    Release-tracker: https://github.com/stashed/CHANGELOG/pull/46
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/stash.appscode.dev/apimachinery/pkg/restic/config.go
+++ vendor/stash.appscode.dev/apimachinery/pkg/restic/config.go
@@ -94,14 +94,6 @@ type SetupOptions struct {
 	IONice         *ofst.IONiceSettings
 }
 
-type MetricsOptions struct {
-	Enabled        bool
-	PushgatewayURL string
-	MetricFileDir  string
-	Labels         []string
-	JobName        string
-}
-
 func NewResticWrapper(options SetupOptions) (*ResticWrapper, error) {
 	wrapper := &ResticWrapper{
 		sh:     shell.NewSession(),
@@ -127,14 +119,14 @@ func NewResticWrapperFromShell(options SetupOptions, sh *shell.Session) (*Restic
 	return wrapper, nil
 }
 
-func (wrapper *ResticWrapper) configure() error {
-	wrapper.sh.SetDir(wrapper.config.ScratchDir)
-	wrapper.sh.ShowCMD = true
-	wrapper.sh.PipeFail = true
-	wrapper.sh.PipeStdErrors = true
+func (w *ResticWrapper) configure() error {
+	w.sh.SetDir(w.config.ScratchDir)
+	w.sh.ShowCMD = true
+	w.sh.PipeFail = true
+	w.sh.PipeStdErrors = true
 
 	// Setup restic environments
-	return wrapper.setupEnv()
+	return w.setupEnv()
 }
 
 func (w *ResticWrapper) SetEnv(key, value string) {
@@ -150,8 +142,12 @@ func (w *ResticWrapper) GetEnv(key string) string {
 	return ""
 }
 
+func (w *ResticWrapper) GetCaPath() string {
+	return w.config.CacertFile
+}
+
 func (w *ResticWrapper) DumpEnv(path string, dumpedFile string) error {
-	if err := os.MkdirAll(path, 0755); err != nil {
+	if err := os.MkdirAll(path, 0o755); err != nil {
 		return err
 	}
 
@@ -161,13 +157,13 @@ func (w *ResticWrapper) DumpEnv(path string, dumpedFile string) error {
 		for k := range w.sh.Env {
 			sortedKeys = append(sortedKeys, k)
 		}
-		sort.Strings(sortedKeys) //sort by key
+		sort.Strings(sortedKeys) // sort by key
 		for _, v := range sortedKeys {
 			envs = envs + fmt.Sprintln(v+"="+w.sh.Env[v])
 		}
 	}
 
-	if err := ioutil.WriteFile(filepath.Join(path, dumpedFile), []byte(envs), 0600); err != nil {
+	if err := ioutil.WriteFile(filepath.Join(path, dumpedFile), []byte(envs), 0o600); err != nil {
 		return err
 	}
 	return nil
@@ -201,9 +197,9 @@ func (w *ResticWrapper) Copy() *ResticWrapper {
 			out.sh.Env[k] = v
 		}
 		// don't use same stdin, stdout, stderr for each instant to avoid data race.
-		//out.sh.Stdin = in.sh.Stdin
-		//out.sh.Stdout = in.sh.Stdout
-		//out.sh.Stderr = in.sh.Stderr
+		// out.sh.Stdin = in.sh.Stdin
+		// out.sh.Stdout = in.sh.Stdout
+		// out.sh.Stderr = in.sh.Stderr
 		out.sh.ShowCMD = w.sh.ShowCMD
 		out.sh.PipeFail = w.sh.PipeFail
 		out.sh.PipeStdErrors = w.sh.PipeStdErrors
